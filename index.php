330d763296a3171d90d745725576bba745596e53
cafundofilms <cafundofilms@gmail.com>
2019-01-20 12:30:11 -0200

    subindo site

---
+++ index.php
@@ -0,0 +1,328 @@
+<?php
+$msg=0;
+@$msg= $_POST['msg'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CafundóFilms</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/resume.min.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+<!--   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css"> -->
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+
+
+  </head>
+
+  <body id="page-top">
+
+            <?php if($msg==enviado): ?>
+          <h1>Mensagem enviada, retornaremos em breve</h1>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top menu-left-style" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger logo-position" href="#page-top">
+<!--         <span class="d-block d-lg-none">Clarence Taylor</span> -->
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile mx-auto mb-2 no-border logo-size" src="img/logo.png" alt="">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav text-style3">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger text-normal" href="#about">Portfólio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger text-normal" href="#experience">Quem somos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger text-normal" href="#education">Contato</a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+          </li> -->
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="p-3 p-lg-5 d-flex d-column head-style" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0 padding-title text-style1">Cafundó
+            <span class="text-primary text-style2">Films</span>
+          </h1>
+
+        </div>
+      </section>
+<section class="mbr-gallery mbr-slider-carousel cid-r6yNW1KMTF background-gallery" id="gallery3-2c">
+
+
+    <div>
+        <div><!-- Filter --><div class="mbr-gallery-filter container gallery-filter-active menu-gallery"><ul buttons="0"><li class="mbr-gallery-filter-all"><a class="btn btn-md btn-primary-outline active display-7 buttons-menu-gallery" href="">Todos</a></li></ul></div><!-- Gallery --><div class="mbr-gallery-row"><div class="mbr-gallery-layout-default"><div><div>
+
+   
+
+   <!-- Início do código de um vídeo -->
+            <div class="mbr-gallery-item mbr-gallery-item--p1 video-slide" data-video-url="https://www.youtube.com/watch?v=fwkKc6M60-0" data-tags="Vídeos"><div href="#lb-gallery3-2c" data-slide-to="0" data-toggle="modal"><div></div><img alt="" src="assets/images/preview.jpg"><span class="icon-video"></span></div></div>
+<!-- Fim dos códigos de um vídeo -->
+
+
+<!-- Só alterar o link do vídeo e da preview da imagem do mesmo -->
+
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1 video-slide" data-video-url="https://www.youtube.com/watch?v=WTrNsAsjEmY" data-tags="Vídeos"><div href="#lb-gallery3-2c" data-slide-to="1" data-toggle="modal"><div></div><img alt="" src="assets/images/preview2.png"><span class="icon-video"></span></div></div>
+
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1 video-slide" data-video-url="https://www.youtube.com/watch?v=t_ULBP6V9bg" data-tags="Vídeos"><div href="#lb-gallery3-2c" data-slide-to="2" data-toggle="modal"><div></div><img alt="" src="assets/images/preview3.jpg"><span class="icon-video"></span></div></div>
+
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1 video-slide" data-video-url="https://www.youtube.com/watch?v=Hp343scZ6kU" data-tags="Vídeos"><div href="#lb-gallery3-2c" data-slide-to="3" data-toggle="modal"><div></div><img alt="" src="assets/images/preview4.jpg"><span class="icon-video"></span></div></div>
+
+
+<!-- Ínicio de um código de imagem -->
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="4" data-toggle="modal"><img src="assets/images/gallery01.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+<!-- Fim de um código de imagem -->
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="5" data-toggle="modal"><img src="assets/images/gallery02.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="6" data-toggle="modal"><img src="assets/images/gallery03.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="7" data-toggle="modal"><img src="assets/images/gallery04.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="8" data-toggle="modal"><img src="assets/images/gallery05.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="9" data-toggle="modal"><img src="assets/images/gallery06.jpg" alt="" title=""><span class="icon-focus"></span></div></div>
+
+            <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Fotos"><div href="#lb-gallery3-2c" data-slide-to="10" data-toggle="modal"><img src="assets/images/gallery07.jpg" alt="" title=""><span class="icon-focus"></span></div></div></div></div><div class="clearfix"></div></div></div><!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery3-2c"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="carousel-inner"><div class="carousel-item active video-container"><img src="assets/images/gallery00.jpg" alt="" title=""></div><div class="carousel-item video-container"><img src="assets/images/gallery00.jpg" alt="" title=""></div><div class="carousel-item video-container"><img src="assets/images/gallery00.jpg" alt="" title=""></div><div class="carousel-item video-container"><img src="assets/images/gallery00.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery01.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery02.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery03.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery04.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery05.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery06.jpg" alt="" title=""></div><div class="carousel-item"><img src="assets/images/gallery07.jpg" alt="" title=""></div></div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery3-2c"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery3-2c"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div></div></div></div></div>
+    </div>
+
+</section>
+      <hr class="m-0">
+
+<!-- div 2 quem somos -->
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column background-gallery " id="experience">
+<section class="team-section my-5 about-style-div background-gallery">
+
+  <!-- Section heading -->
+  <h2 class="h1-responsive font-weight-bold text-center my-5 tittle-we-are">Quem
+                <span class="text-primary tittle-we-are2">Somos</span>
+  </h2>
+  <!-- Section description -->
+<!--   <p class="grey-text text-center w-responsive mx-auto mb-5">Dois amigos</p> -->
+  <!-- Grid row-->
+  <div class="row text-center text-md-left">
+
+    <!-- Grid column -->
+    <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="C:\Users\denis\Documents\site1\startbootstrap-resume-gh-pages\assets\images\brito.png" class="rounded z-depth-1 avatar-size" alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3 text-we-are1">Daniel Brito</h4>
+        <h6 class="font-weight-bold grey-text mb-3 text-we-are2">VideoMaker</h6>
+        <p class="grey-text text-we-are3">Mais de trinta anos como cinegrafista, visitando países como: África do Sul, Madagascar, Costa Rica, Chile, Noruega, Finlândia, Suécia e Panamá. Trabalhos importantes: Cobertura do papa em Cuba, Copa do mundo no Brasil, Altas hora da Globo, Encontro com Fátima Bernardes, Faustão, Lata velha, Caldeirão do huck, Masterchef e Rall dos Sertões.</p>
+        <!-- Facebook-->
+        <a class="p-2 fa-lg fb-ic">
+          <i class="fa fa-facebook"> </i>
+        </a>
+        <!-- Twitter -->
+        <a class="p-2 fa-lg tw-ic">
+          <i class="fa fa-twitter"> </i>
+        </a>
+        <!--Dribbble -->y
+        <a class="p-2 fa-lg dribbble-ic">
+          <i class="fa fa-dribbble"> </i>
+        </a>
+      </div>
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+      <div class="avatar mb-md-0 mb-4 mx-4">
+        <img src="C:\Users\denis\Documents\site1\startbootstrap-resume-gh-pages\assets\images\rogerio.png" class="rounded z-depth-1 avatar-size" alt="Sample avatar">
+      </div>
+      <div class="mx-4">
+        <h4 class="font-weight-bold mb-3 text-we-are1">Rogério Voltan</h4>
+        <h6 class="font-weight-bold grey-text mb-3 text-we-are2">Photographer</h6>
+        <p class="grey-text text-we-are3">Fotógrafo profissional desde 1994. Trabalha para algumas revistas no Brasil: CASA E COMIDA, ALFA, CASA E JARDIM, VEJA, VIAGEM E TURISMO, PRAZERES DA MESA, ELLE, ESTILO, GOURMET, IATE, CLAUDIA. E internacionalmente em: Espanha, Colômbia, França, Chile, Itália, e Alemanha.</p>
+        <!-- Facebook-->
+        <a class="p-2 fa-lg fb-ic">
+          <i class="fa fa-facebook"> </i>
+        </a>
+        <!--YouTube -->
+        <a class="p-2 fa-lg yt-ic">
+          <i class="fa fa-youtube"> </i>
+        </a>
+        <!--Instagram-->
+        <a class="p-2 fa-lg ins-ic">
+          <i class="fa fa-instagram"> </i>
+        </a>
+      </div>
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row-->
+
+  <!-- Grid row-->
+<!--   <div class="row text-center text-md-left">
+  </div> -->
+  <!-- Grid row-->
+
+</section>
+<!-- Section: Team v.3 -->
+
+      </section>
+
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column background-gallery" id="education">
+      
+      <fieldset>
+      <form action="mail_send.php" method="post">
+            
+                <label for name="nome">Nome: </label>
+                <input id="nome" name="nome" type="text" required><br>
+
+                <label for name="email">E-mail: </label>
+                <input id="email" name="email" type="email" required><br>
+
+                <label for name="telefone">Telefone: </label>
+                <input id="telefone" name="telefone" type="tel" required><br>
+
+                <label for name="mensagem">Mensagem: </label>
+                <input id="mensagem" name="mensagem" required><br>
+
+                <input type="submit">
+            
+        </form>
+        </fieldset>
+
+         
+</section>
+
+      </section>
+
+<!--       <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+ Quarta area --><!--       </section> --> 
+<!-- 
+      <hr class="m-0"> -->
+
+<!--       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <div class="my-auto">
+          <h2 class="mb-5">Interests</h2>
+          <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
+          <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+        </div>
+      </section> -->
+<!-- 
+      <hr class="m-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+        <div class="my-auto">
+          <h2 class="mb-5">Awards &amp; Certifications</h2>
+          <ul class="fa-ul mb-0">
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Google Analytics Certified Developer</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              Mobile Web Specialist - Google Certification</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              2<sup>nd</sup>
+              Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              1<sup>st</sup>
+              Place - James Buchanan High School - Hackathon 2006</li>
+            <li>
+              <i class="fa-li fa fa-trophy text-warning"></i>
+              3<sup>rd</sup>
+              Place - James Buchanan High School - Hackathon 2005</li>
+          </ul>
+        </div>
+      </section> -->
+
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/resume.min.js"></script>
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/gallery/player.min.js"></script>
+  <script src="assets/gallery/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i></i></a></div>
+  </body>
+
+</html>
---
+++ mail_send.php
@@ -0,0 +1,31 @@
+<?php
+
+$para= "cafundofilms@gmail.com"
+$assunto="Mensagem do site"
+$nome= $_POST['nome'];
+$email= $_POST['email'];
+$telefone= $_POST['telefone'];
+$mensagem= $_POST['mensagem'];
+
+		$corpo="<strong> Mensagem do site </strong><br><br>";
+		$corpo.="<strong><br>Nome </strong> $nome";
+		$corpo.="<strong><br> Mensagem do site </strong> $fome";
+		$corpo.="<strong><br> Mensagem do site </strong> $email";
+		$corpo.="<strong><br> Mensagem do site </strong> $mensagem";
+
+		$header="From: $email Reply-to: $email";
+		$header="Content-Type: text/html; charset= utf-8";
+
+mail($para,$assunto,$corpo,$header);
+
+
+// if ($nome && validaEmail($de) && $mensagem) {
+//     enviaEmail($de, $assunto, $mensagem, $para, $email_servidor);
+//     $pagina = "mail_ok.php";
+// } else {
+//     $pagina = "mail_error.php";
+// }
+
+header("location:$index.php?msg=enviado");
+
+?>
